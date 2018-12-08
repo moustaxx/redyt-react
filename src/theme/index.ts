657a46718a563dc997f6099d11d2fb4335f594e8
moustaxx <maciasm1303@gmail.com>
2018-12-08 18:59:06 +0100

    Edit theme colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -14,16 +14,6 @@ export interface IThemeInterface {
 	borderColor: string;
 }
 
-// export const theme = {
-// 	mainSubforumColor: 'hsl(200, 21%, 39%)',
-// 	secondarySubforumColor: 'hsl(206, 100%, 41%)',
-// 	tertiarySubforumColor: 'hsl(169, 44%, 36%)',
-// 	fontColor: '#1a1a1b',
-// 	iconColor: '#878a8c',
-// 	mainBgColor: '#dae0e6',
-// 	borderColor: '#cccccc'
-// };
-
 export const theme = {
 	mainSubforumColor: 'hsl(200, 21%, 39%)',
 	secondarySubforumColor: 'hsl(206, 100%, 41%)',
@@ -35,8 +25,18 @@ export const theme = {
 	searchBoxBgColor: '#212121',
 	buttonBgColor: 'hsl(169,44%,36%)',
 	secondButtonBgColor: 'hsl(204, 7%, 85%)',
-	borderColor: '#4e4e4e'
+	borderColor: '#3c3c3c'
 };
+
+// export const theme = {
+// 	mainSubforumColor: 'hsl(200, 21%, 39%)',
+// 	secondarySubforumColor: 'hsl(206, 100%, 41%)',
+// 	tertiarySubforumColor: 'hsl(169, 44%, 36%)',
+// 	fontColor: '#1a1a1b',
+// 	iconColor: '#878a8c',
+// 	mainBgColor: '#dae0e6',
+// 	borderColor: '#cccccc'
+// };
 
 const {
 	default: styled,
