b7c78e7746ff0830660775899a4666e73b484325
moustaxx <maciasm1303@gmail.com>
2018-12-25 11:05:20 +0100

    Move TopBar styles to separate file

---
+++ src/components/TopBar/TopBar.style.tsx
@@ -0,0 +1,29 @@
+import styled from 'Theme/';
+
+import { niceButton, invertedButton } from 'Components/Shared/Button/Button';
+
+export const StyledTopBar = styled.div`
+	background: ${props => props.theme.secondaryBgColor};
+	position: sticky;
+	top: 0;
+	width: 100%;
+	height: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-bottom: 1px solid ${props => props.theme.mainBgColor};
+	z-index: 10;
+	box-sizing: border-box;
+	padding: 0 10px;
+`;
+
+export const Button = styled(niceButton)`
+	width: 120px;
+	margin: 0 5px;
+	flex-shrink: 0;
+`;
+export const Button2 = styled(invertedButton)`
+	width: 120px;
+	margin: 0 5px;
+	flex-shrink: 0;
+`;
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,37 +1,9 @@
 import * as React from 'react';
-import styled from 'Theme/';
 
 import Logo from './Logo/Logo';
 import SearchBox from './SearchBox/SearchBox';
 import NavMenu from './NavMenu/NavMenu';
-import {niceButton, invertedButton} from 'Components/Shared/Button/Button';
-
-
-const StyledTopBar = styled.div`
-	background: ${props => props.theme.secondaryBgColor};
-	position: sticky;
-	top: 0;
-	width: 100%;
-	height: 50px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	border-bottom: 1px solid ${props => props.theme.mainBgColor};
-	z-index: 10;
-	box-sizing: border-box;
-	padding: 0 10px;
-`;
-
-const Button = styled(niceButton)`
-	width: 120px;
-	margin: 0 5px;
-	flex-shrink: 0;
-`;
-const Button2 = styled(invertedButton)`
-	width: 120px;
-	margin: 0 5px;
-	flex-shrink: 0;
-`;
+import { StyledTopBar, Button, Button2 } from './TopBar.style';
 
 class TopBar extends React.Component {
 	public render() {
