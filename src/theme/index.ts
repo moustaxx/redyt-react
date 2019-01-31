f7d372b0c99f2625964d07bff8fd11e1524c6eb1
moustaxx <maciasm1303@gmail.com>
2019-01-31 15:44:29 +0100

    Restructurise theme folder

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,42 +1,5 @@
 import * as styledComponents from 'styled-components';
-
-export interface IThemeInterface {
-	mainSubforumColor: string;
-	secondarySubforumColor: string;
-	tertiarySubforumColor: string;
-	fontColor: string;
-	iconColor: string;
-	mainBgColor: string;
-	secondaryBgColor: string;
-	searchBoxBgColor: string;
-	buttonBgColor: string;
-	secondButtonBgColor: string;
-	borderColor: string;
-}
-
-export const theme = {
-	mainSubforumColor: 'hsl(200, 21%, 39%)',
-	secondarySubforumColor: 'hsl(206, 100%, 41%)',
-	tertiarySubforumColor: 'hsl(169, 44%, 36%)',
-	fontColor: '#b3b3b3',
-	iconColor: '#878a8c',
-	mainBgColor: '#212121',
-	secondaryBgColor: '#333333',
-	searchBoxBgColor: '#212121',
-	buttonBgColor: 'hsl(169,44%,36%)',
-	secondButtonBgColor: 'hsl(204, 7%, 85%)',
-	borderColor: '#3c3c3c'
-};
-
-// export const theme = {
-// 	mainSubforumColor: 'hsl(200, 21%, 39%)',
-// 	secondarySubforumColor: 'hsl(206, 100%, 41%)',
-// 	tertiarySubforumColor: 'hsl(169, 44%, 36%)',
-// 	fontColor: '#1a1a1b',
-// 	iconColor: '#878a8c',
-// 	mainBgColor: '#dae0e6',
-// 	borderColor: '#cccccc'
-// };
+import { theme, IThemeInterface } from './theme';
 
 const {
 	default: styled,
@@ -47,5 +10,4 @@ const {
 } = styledComponents as styledComponents.ThemedStyledComponentsModule<IThemeInterface>;
 
 export default styled;
-export { css, createGlobalStyle, keyframes, ThemeProvider };
-
+export { theme, css, createGlobalStyle, keyframes, ThemeProvider };
---
+++ src/theme/theme.ts
@@ -0,0 +1,39 @@
+export interface IThemeInterface {
+	mainSubforumColor: string;
+	secondarySubforumColor: string;
+	tertiarySubforumColor: string;
+	fontColor: string;
+	iconColor: string;
+	mainBgColor: string;
+	secondaryBgColor: string;
+	searchBoxBgColor: string;
+	buttonBgColor: string;
+	secondButtonBgColor: string;
+	borderColor: string;
+	defaultFont: string;
+}
+
+export const theme = {
+	mainSubforumColor: 'hsl(200, 21%, 39%)',
+	secondarySubforumColor: 'hsl(206, 100%, 41%)',
+	tertiarySubforumColor: 'hsl(169, 44%, 36%)',
+	fontColor: '#cecece',
+	iconColor: '#878a8c',
+	mainBgColor: '#232228',
+	secondaryBgColor: '#2b2a30',
+	searchBoxBgColor: '#232228',
+	buttonBgColor: 'hsl(169,44%,36%)',
+	secondButtonBgColor: 'hsl(204, 7%, 85%)',
+	borderColor: '#3c3c3c',
+	defaultFont: 'Source Sans Pro'
+};
+
+// export const theme = {
+// 	mainSubforumColor: 'hsl(200, 21%, 39%)',
+// 	secondarySubforumColor: 'hsl(206, 100%, 41%)',
+// 	tertiarySubforumColor: 'hsl(169, 44%, 36%)',
+// 	fontColor: '#1a1a1b',
+// 	iconColor: '#878a8c',
+// 	mainBgColor: '#dae0e6',
+// 	borderColor: '#cccccc'
+// };
