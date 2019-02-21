92603bc92e32330c31dd4efe2192957d256fb24a
moustaxx <maciasm1303@gmail.com>
2019-02-21 14:53:57 +0100

    Fix Post <a> in <a> error

--- src/components/Forum/Posts/Post/Post.tsx
+++ src/components/Forum/Posts/Post/Post.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Vote from 'Components/UI/Vote/Vote';
-import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
+import { withRouter, RouteComponentProps, Link } from 'react-router-dom';
 import { IPost } from '../Posts.apollo';
 import { StyledPost, Cnt, FirstLine, PostContent, Stats, PostDate } from './Post.style';
 import { MdComment, MdMoreHoriz } from 'react-icons/md';
@@ -12,36 +12,41 @@ interface IPostProps extends RouteComponentProps<{ subforumName: string }> {
 
 class Post extends React.Component<IPostProps> {
 	public render() {
+
 		const { id, title, author, createdAt } = this.props.post;
 		const date = new Date(createdAt).toLocaleString();
+		const subforumName = this.props.match.params.subforumName;
+
 		return (
+			<StyledPost onClick={() => this.props.history.push(subforumName + '/' + id)}>
 			<Link to={`${this.props.match.params.subforumName}/${id}`} >
 				<StyledPost>
 					<Vote className='voteHere'/>
-					<Cnt>
-						<FirstLine>
-							<PostContent>
-								<div className='postHeading'>
-									{/* <div className='tag' /> */}
-									<span className='postTitle'>{title}</span>
-									<span className='attachedLink' />
-								</div>
-							</PostContent>
-							<Stats>
+				<Cnt>
+					<FirstLine>
+						<PostContent>
+							<div className='postHeading'>
+								<div className='tag' />
+								<Link to={id} className='postTitle'>{title}</Link>
+								<span className='attachedLink' />
+							</div>
+						</PostContent>
+						<Stats>
 								<TiAttachment className='attachmentIcon' />
-								<div className='comments'>
-									<MdComment className='commentIcon' />
+							<div className='comments'>
+								<MdComment className='commentIcon' />
 									<div className='comNumber'>5</div>
-								</div>
-								<MdMoreHoriz className='postOptionsIcon' />
-							</Stats>
-						</FirstLine>
-						<PostDate>
-							<span>Posted by <Link to={`/user/${author.name}`}>{author.name}</Link> {date}</span>
-						</PostDate>
-					</Cnt>
-				</StyledPost>
-			</Link>
+							</div>
+							<MdMoreHoriz className='postOptionsIcon' />
+						</Stats>
+					</FirstLine>
+					<PostDate>
+						<span>
+							Posted by <Link onClick={e => e.stopPropagation()} to={'/user/' + author.name}>{author.name}</Link> {date}
+						</span>
+					</PostDate>
+				</Cnt>
+			</StyledPost>
 		);
 	}
 }
