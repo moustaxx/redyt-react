dec63249f4ed4efc13198a8e778a4156e980b778
moustaxx <maciasm1303@gmail.com>
2019-03-23 15:59:43 +0100

    Show Post Discussion on no comments situations

--- src/components/PostWindow/Left/Discussion/Discussion.tsx
+++ src/components/PostWindow/Left/Discussion/Discussion.tsx
@@ -16,18 +16,20 @@ const Discussion = ({ postID }: IDiscussionProps) => {
 	const { data, loading, error } = useQuery<IGetCommentsByPostRes>(GET_COMMENTS, { variables: { postID } });
 	if (loading) return <LoadingSpinner />;
 	if (error) return <CommentError>Error! Could not show comments!</CommentError>;
-	if (!data || !data.getCommentsByPost.length) return null;
+	if (!data) return null;
 	return (
 		<StyledDiscussion>
 			<NotLoggedIn />
-			<SortOptions>
-				<button className='sort' id='sortButton'>
-					<span>Sort</span>
-					<span className='slide'>HOT</span>
-					<MdArrowDropDown />
-				</button>
-				{/* <SortMenu /> */}
-			</SortOptions>
+			{data.getCommentsByPost.length ?
+				<SortOptions>
+					<button className='sort' id='sortButton'>
+						<span>Sort</span>
+						<span className='slide'>HOT</span>
+						<MdArrowDropDown />
+					</button>
+					{/* <SortMenu /> */}
+				</SortOptions>
+			: null}
 			<div className='comments'>
 				{data.getCommentsByPost.map(({ id, content, author, createdAt }) =>
 					<Comment key={id} data={{ id, content, author, createdAt}} />
