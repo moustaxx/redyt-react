e83264ea7307607edd1e57a30715f679a33f5067
moustaxx <maciasm1303@gmail.com>
2019-04-12 12:57:48 +0200

    Remove apollo-link-state

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,28 +1,11 @@
 import { ApolloClient } from 'apollo-client';
-
 import { ApolloLink } from 'apollo-link';
 import { onError } from 'apollo-link-error';
 import { BatchHttpLink } from 'apollo-link-batch-http';
-
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { withClientState } from 'apollo-link-state';
-
-// import defaults from './state/defaults';
-// import mutations from './state/mutations';
-// import queries from './apollo/state/resolvers/queries';
-
-const cache = new InMemoryCache();
-const stateLink = withClientState({
-	cache,
-	// defaults,
-	resolvers: {
-		// Query: { ...queries },
-		// Mutation: { ...mutations },
-	}
-});
 
 const client = new ApolloClient({
-	cache,
+	cache: new InMemoryCache(),
 	link: ApolloLink.from([
 		onError(({ graphQLErrors, networkError }) => {
 			if (graphQLErrors)
@@ -31,13 +14,11 @@ const client = new ApolloClient({
 				);
 			if (networkError) console.log(`[Network error (logout)]: ${networkError}`);
 		}),
-		stateLink,
 		new BatchHttpLink({
 			uri: 'http://localhost:4000/graphql',
 			credentials: 'include'
 		})
 	])
 });
-client.onResetStore(stateLink.writeDefaults as any);
 
 export default client;
