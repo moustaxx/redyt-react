7463e836b0b15276467c43a89d85415df944348d
moustaxx <maciasm1303@gmail.com>
2019-04-19 12:42:44 +0200

    Fix Vote component classes aligned from props

--- src/components/UI/Vote/Vote.tsx
+++ src/components/UI/Vote/Vote.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { MdThumbUp, MdThumbDown } from 'react-icons/md';
+import clsx from 'clsx';
+
 import voteStyles from './Vote.style';
 
 const Vote = ({ className, voteBalance = 0 }: { className?: string; voteBalance?: number }) => {
 	const classes = voteStyles();
 	return (
-		<div className={classes.root}>
+		<div className={clsx(classes.root, className)}>
 			<MdThumbUp className={classes.voteUp} />
 			<div className={classes.voteSummary}>{voteBalance}</div>
 			<MdThumbDown className={classes.voteDown} />
