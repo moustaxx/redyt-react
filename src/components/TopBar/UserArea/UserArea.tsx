07e4d6caf8579c7c764ee4fd6b8a60f6d31b3cf6
moustaxx <maciasm1303@gmail.com>
2019-04-23 17:51:40 +0200

    Remove useless try-catch from UserArea component

--- src/components/TopBar/UserArea/UserArea.tsx
+++ src/components/TopBar/UserArea/UserArea.tsx
@@ -12,20 +12,19 @@ const UserArea = () => {
 	const toggleNavStatus = () => setNavStatus(!isDropDownOpen);
 
 	const classes = userAreaStyles();
-	try {
-		const { data, loading, error } = useQuery<IUserRes>(GET_SESSION_OWNER);
-		if (loading) return <div>Loading...</div>;
-		if (error) console.log('UserArea:', error);
-		if (!data) return null;
-		return (
-			<div className={classes.root} onClick={toggleNavStatus}>
-				<div className={classes.avatar}>{data.getSessionOwner.name[0]}</div>
-				<span>{data.getSessionOwner.name}</span>
-				<MdArrowDropDown />
-				{isDropDownOpen ? <UserDropDown username={data.getSessionOwner.name} /> : null}
-			</div>
-		);
-	} catch { return null; }
+
+	const { data, loading, error } = useQuery<IUserRes>(GET_SESSION_OWNER);
+	if (loading) return <div>Loading...</div>;
+	if (error || !data) return null;
+
+	return (
+		<div className={classes.root} onClick={toggleNavStatus}>
+			<div className={classes.avatar}>{data.getSessionOwner.name[0]}</div>
+			<span>{data.getSessionOwner.name}</span>
+			<MdArrowDropDown />
+			{isDropDownOpen && <UserDropDown username={data.getSessionOwner.name} />}
+		</div>
+	);
 };
 
 export default UserArea;
