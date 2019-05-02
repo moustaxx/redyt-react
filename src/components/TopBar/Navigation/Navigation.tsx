e4dcebf3e02a53d399fcd18f198cdecefbec835f
moustaxx <maciasm1303@gmail.com>
2019-05-02 15:12:06 +0200

    Refactor query in Navigation to hooks

--- src/components/TopBar/Navigation/Navigation.tsx
+++ src/components/TopBar/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Query } from 'react-apollo';
+import { useQuery } from 'react-apollo-hooks';
 import { MdArrowDropDown, MdAccountBox, MdSentimentVeryDissatisfied, MdAccessTime } from 'react-icons/md';
 import { withRouter, RouteComponentProps } from 'react-router';
 import clsx from 'clsx';
@@ -19,40 +19,34 @@ const Navigation = (props: RouteComponentProps) => {
 
 	const toggleNavStatus = () => setNavStatus(!isNavMenuOpen);
 	const stopPropagation = (e: Event) => e.stopPropagation();
-
+	
 	const classes = navigationStyles();
+
 	const skip = dispatcher !== 'r';
+	const { data, loading, error } = useQuery<IGetSubforumRes>(GET_SUBFORUM, { variables: { name }, skip });
+
+	let subforumName = 'Loading...';
+	let Icon = MdAccessTime;
+
+	if (!loading) {
+		switch (dispatcher) {
+			case 'user': Icon = MdAccountBox; break;
+			case 'r': Icon = SubforumIcon; break;
+			default: Icon = MdSentimentVeryDissatisfied;
+		}
+
+		if (skip && name) subforumName = name;
+		else if (data && !error) subforumName = data.getSubforum.name;
+		else subforumName = 'Error... :(';
+	}
+
 	return (
 		<div className={clsx(classes.root, isNavMenuOpen && 'active')}>
 			<div className={classes.cnt} onClick={toggleNavStatus}>
-				<Query<IGetSubforumRes> query={GET_SUBFORUM} variables={{ name }} skip={skip}>{
-					({ loading, error, data }) => {
-						let nameee: string;
-						let Icon = MdAccessTime;
-
-						if (loading) nameee = 'Loading...';
-						else if (!data || error) {
-							nameee = 'Error... :(';
-							Icon = MdSentimentVeryDissatisfied;
-						}
-						else {
-							if (skip) nameee = name;
-							else nameee = data.getSubforum.name;
-
-							switch (dispatcher) {
-								case 'user': Icon = MdAccountBox; break;
-								case 'r': Icon = SubforumIcon;
-							}
-						}
-
-						return (
-							<div className={classes.cnt2}>
-								<div className={classes.navIcon}><Icon /></div>
-								<div className={classes.subforumName}>{nameee}</div>
-							</div>
-						);
-					}
-				}</Query>
+				<div className={classes.cnt2}>
+					<div className={classes.navIcon}><Icon /></div>
+					<div className={classes.subforumName}>{subforumName}</div>
+				</div>
 				<MdArrowDropDown size={20} />
 			</div>
 			{isNavMenuOpen &&
