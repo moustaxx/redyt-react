8e400dc452005010db16124d356078c3c7e979bc
moustaxx <maciasm1303@gmail.com>
2019-05-06 18:54:57 +0200

    Add log out

---
+++ src/components/TopBar/UserArea/UserDropDown/UserDropDown.apollo.ts
@@ -0,0 +1,21 @@
+import gql from 'graphql-tag';
+
+export const LOG_OUT = gql`
+	mutation {
+		logOut {
+			name
+		}
+	}
+`;
+
+export interface IUser {
+	id: string;
+	name: string;
+	email: string;
+	updatedAt: Date;
+	createdAt: Date;
+}
+
+export interface IUserRes {
+	logOut: IUser;
+}
--- src/components/TopBar/UserArea/UserDropDown/UserDropDown.tsx
+++ src/components/TopBar/UserArea/UserDropDown/UserDropDown.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { MdAccountBox, MdSettings, MdWbSunny, MdPowerSettingsNew } from 'react-icons/md';
-import dropDownStyles from './UserDropDown.style';
+import { useMutation, useApolloClient } from 'react-apollo-hooks';
+
+import { LOG_OUT, IUserRes } from './UserDropDown.apollo';
 import { SetThemeContext } from '../../../../index';
+import dropDownStyles from './UserDropDown.style';
+
+import { MdAccountBox, MdSettings, MdWbSunny, MdPowerSettingsNew } from 'react-icons/md';
 import DropDown from 'Components/UI/DropDown/DropDown';
 
 interface IUserDropDown {
@@ -11,6 +15,15 @@ interface IUserDropDown {
 
 const UserDropDown = ({ username }: IUserDropDown) => {
 	const classes = dropDownStyles();
+
+	const setTheme = React.useContext(SetThemeContext);
+	const logOut = useMutation<IUserRes>(LOG_OUT);
+	const client = useApolloClient();
+	const handleLogOut = async () => {
+		await logOut();
+		client.resetStore();
+	};
+
 	return (
 		<DropDown className={classes.root}>
 			<div className={classes.heading}>Me</div>
@@ -24,19 +37,14 @@ const UserDropDown = ({ username }: IUserDropDown) => {
 			</Link>
 
 			<div className={classes.heading}>View settings</div>
-			<SetThemeContext.Consumer>
-				{setTheme => (
-					// tslint:disable-next-line: jsx-no-lambda
-					<a onClick={() => setTheme('toggle')}>
-						<MdWbSunny />
-						<span>Dark mode</span>
-					</a>
-				)}
-			</SetThemeContext.Consumer>
-			<Link to={'/'}>
+			<a onClick={() => setTheme('toggle') /* tslint:disable-line: jsx-no-lambda */}>
+				<MdWbSunny />
+				<span>Dark mode</span>
+			</a>
+			<a onClick={handleLogOut}>
 				<MdPowerSettingsNew />
 				<span>Log out</span>
-			</Link>
+			</a>
 
 		</DropDown>
 	);
